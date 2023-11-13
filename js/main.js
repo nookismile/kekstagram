41007c4b096d5721ccfb026317f06796e9b36e1b
nookismile <52919340+nookismile@users.noreply.github.com>
2023-11-13 15:28:22 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,13 +1,51 @@
 import './big-picture.js';
 import './editor.js';
 import './effects.js';
-import './validation.js'
+import './validation.js';
+import { shuffleArray, debounce } from './util.js';
 import { renderPreviews } from './previews.js';
 import { request } from './fetch.js';
 import { showError } from './alerts.js';
 
+const DEFAULT_PREVIEW_LOAD = 25;
+const RANDOM_PREVIEW_LOAD = 10;
+
+const filter = document.querySelector('.img-filters');
+let photos = [];
+
+function removeActiveClass() {
+  let activeFilter = document.querySelector('.img-filters__button--active');
+  activeFilter.classList.remove('img-filters__button--active');
+}
+
+const removePhotos = () => {
+  const images = document.querySelectorAll('.picture');
+  if (images) {
+    images.forEach(element => {
+      element.remove();
+    });
+  }
+}
+
+const filters = {
+  'filter-default': () => {
+    renderPhotos(photos.slice(0, DEFAULT_PREVIEW_LOAD))
+  },
+  'filter-random': () => {
+    renderPhotos(shuffleArray(photos.slice()).slice(0, RANDOM_PREVIEW_LOAD));
+
+  },
+  'filter-discussed': () => {
+    renderPhotos(photos.slice().sort((a, b) => {
+      return b.comments.length - a.comments.length;
+    }))
+  },
+}
+
 const onSuccess = (data) => {
-  renderPreviews(data.slice());
+  filter.classList.remove('img-filters--inactive');
+  photos = data.slice();
+  renderPreviews(photos.slice(), DEFAULT_PREVIEW_LOAD);
 }
 
 const onError = () => {
@@ -16,4 +54,14 @@ const onError = () => {
 
 request(onSuccess,onError, 'GET');
 
+const onFilterClick = debounce((evt) => {
+  if (evt.target.classList.contains('img-filters__button')) {
+    removeActiveClass();
+    removePhotos();
+    evt.target.classList.add('img-filters__button--active')
+    filters[evt.target.id]()
+  }
+})
+
+filter.addEventListener('click', onFilterClick)
 
--- js/util.js
+++ js/util.js
@@ -26,4 +26,32 @@ const isEnterEvent = (evt) => {
   return evt.key === 'Enter';
 };
 
-export { getRandomInt, getRandomIntArr, stringCount, isEscEvent, isEnterEvent };
+// перемешиваем массив
+const shuffleArray = (arr) => {
+  let j, temp;
+  for (let i = arr.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = arr[j];
+    arr[j] = arr[i];
+    arr[i] = temp;
+  }
+  return arr;
+}
+
+// таймаут запроса
+const DEBOUNCE_INTERVAL = 500
+
+const debounce = (cb) => {
+  let lastTimeout = null;
+
+  return (...args) => {
+    if (lastTimeout) {
+      window.clearTimeout(lastTimeout);
+    }
+    lastTimeout = window.setTimeout(() => {
+      cb(...args);
+    }, DEBOUNCE_INTERVAL);
+  };
+};
+
+export { getRandomInt, getRandomIntArr, stringCount, isEscEvent, isEnterEvent, shuffleArray, debounce };
